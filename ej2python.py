bdfb4e422780054c7835a8476185abe71214086c
Bruno <163897566+benyitang@users.noreply.github.com>
2024-05-06 17:27:55 -0300

    Create ej2python.py

---
+++ ej2python.py
@@ -0,0 +1,11 @@
+def sumaEjercicio2():
+    n1 = int(input("Ingrese un numero entero"))
+    n2 = int(input("Ingrese otro numero entero"))
+    n3 = int(input("Ingrese otro numero entero"))
+    n4 = int(input("Ingrese otro numero entero"))
+
+    resultado = n1 + n2 + n3 + n4
+     
+    print("El resultado es: ", resultado)
+
+sumaEjercicio2()
