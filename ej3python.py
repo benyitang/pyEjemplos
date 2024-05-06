d90258aa1866d3ab34cc2b57ea1ce97eef0f33ea
Bruno <163897566+benyitang@users.noreply.github.com>
2024-05-06 17:27:58 -0300

    Create ej3python.py

---
+++ ej3python.py
@@ -0,0 +1,9 @@
+def multiEjercicio3():
+    n1 = float(input("Ingrese un numero entero"))
+    n2 = float(input("Ingrese otro numero entero"))
+
+    resultado = n1 * n2
+     
+    print("El resultado es: ", resultado)
+
+multiEjercicio3()
