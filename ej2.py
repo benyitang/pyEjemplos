b3a241369df987ea3ed29bf686b2397494c578e6
Bruno <163897566+benyitang@users.noreply.github.com>
2024-05-06 17:27:53 -0300

    Create ej2.py

---
+++ ej2.py
@@ -0,0 +1,18 @@
+#A partir del siguiente array que se proporciona: var valores = [true, 5, false, "hola", "adios",2];
+#Determinar cual de los dos elementos de texto es mayor
+#Utilizando exclusivamente los dos valores booleanos del array, determinar los operadores
+#necesarios para obtener un resultado true y otro resultado false
+#Determinar el resultado de las cinco operaciones matemáticas realizadas con los dos
+#elementos numéricos
+
+val = [True, 5 ,False ,"hola","adios", 2]
+if len(val[3]) < len(val[4]):
+    print ("adios es mayor que hola.")
+else:
+    print ("adios es menor que hola.")
+
+print(val[1] + val[5])
+print(val[1] - val[5])
+print(val[1] * val[5])
+print(val[1] / val[5])
+
