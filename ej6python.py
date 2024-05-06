4f6b822fc863bd2ab31be6d524a7a84c8ec5b091
Bruno <163897566+benyitang@users.noreply.github.com>
2024-05-06 17:28:05 -0300

    Create ej6python.py

---
+++ ej6python.py
@@ -0,0 +1,9 @@
+def Ejercicio6():
+
+  ladoA= float(input("Ingrese medida "))
+  ladoB = float(input("Ingrese medida "))
+  resultado = ladoA * ladoB / 2
+    
+  print("Su medida es: ", resultado)
+
+Ejercicio6()
