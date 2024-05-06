b9362749582d5cdd9f25ac8bb74e521cfd8efac1
Bruno <163897566+benyitang@users.noreply.github.com>
2024-05-06 17:27:51 -0300

    Create ej1python.py

---
+++ ej1python.py
@@ -0,0 +1,15 @@
+#Leer 2 números enteros, calcular la suma e informar el resultado.
+
+def sumaEjercicio1():
+    n1 = int(input("Ingrese un numero entero"))
+    n2 = int(input("Ingrese otro numero entero"))
+
+    resultado = n1 + n2
+     
+    print("El resultado es: ", resultado)
+
+sumaEjercicio1()
+
+
+
+
