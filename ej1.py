df0e5c9af3e4d28049b64b5d8622000ad38c015d
Bruno <163897566+benyitang@users.noreply.github.com>
2024-05-06 17:27:47 -0300

    Create ej1.py

---
+++ ej1.py
@@ -0,0 +1,8 @@
+#Crear una lista llamado meses y que almacene el nombre de los doce meses del año
+#Mostrar por pantalla los doce nombres utilizando la funcion print().
+
+
+meses = ["Enero", "febrero", "marzo", "abril", "mayo", "junio", "julio", "agosto", "septiembre", "octubre", "noviembre" ,"diciembre"]
+
+for mes in meses:
+    print(meses)
