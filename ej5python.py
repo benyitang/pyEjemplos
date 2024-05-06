593d5bd11e0a14de40b399bfdf5aed077b5108c4
Bruno <163897566+benyitang@users.noreply.github.com>
2024-05-06 17:28:03 -0300

    Create ej5python.py

---
+++ ej5python.py
@@ -0,0 +1,13 @@
+def Ejercicio5():
+
+    seg = float(input("Ingrese segundos"))
+    min = float(input("Ingrese minutos"))
+    hrs = float(input("Ingrese horas"))
+    resultado = hrs * 3600
+    resultado2 = min * 60
+     
+    Ensegundos = resultado + resultado2 + seg
+
+    print("El resultado expresado es: ", resultado)
+
+Ejercicio5() 
