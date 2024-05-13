1d3144a5bdc3f7094acdb16869e469c74c256490
Bruno <163897566+benyitang@users.noreply.github.com>
2024-05-13 14:19:23 -0300

    ej10

---
+++ ej10.py
@@ -0,0 +1,12 @@
+def Ejercicio10():
+    a = int(input("Ingrese la calificación de exámen parcial."))
+    b = int(input("Ingrese la calificación por TPs."))
+    c = int(input("Ingrese la calificación del exámen integrador."))
+
+    calificacionA = a * 0.3
+    calificacionB = b * 0.2
+    calificacionC = c * 0.5
+
+    notafinal = calificacionA + calificacionB + calificacionC
+    print("El promedio del estudiante es: "+ notafinal)
+Ejercicio10
