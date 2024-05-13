b737805529b91ed203777acd0bc99183e6678744
Bruno <163897566+benyitang@users.noreply.github.com>
2024-05-13 14:19:05 -0300

    ej7

---
+++ ej7.py
@@ -0,0 +1,20 @@
+def Ejercicio7():
+    n1 = input("Ingrese un número.")
+    n1 = int(n1)
+    n2 = input("Ingrese un segundo número.")
+    n2 = int(n2)
+    n3 = input("Ingrese un tercer número.")
+    n3 = int(n3)
+    n4 = input("Ingrese un cuarto número.")
+    n4 = int(n4)
+    n5 = input("Ingrese un quinto número.")
+    n5 = int(n5)
+    n6 = input("Ingrese el último número.")
+    n6 = int(n6)
+
+    total = n1+ n2 + n3 + n4 + n5 + n6
+
+    promedio = total/6
+
+    print("El promedio es: "+ promedio)
+Ejercicio7()
