83175db85821b0394ee93c002c9c9f35a50fd955
Bruno <163897566+benyitang@users.noreply.github.com>
2024-05-13 14:19:11 -0300

    ej8

---
+++ ej8.py
@@ -0,0 +1,8 @@
+def Ejercicio8():
+    n1 = int(input("Ingrese un número."))
+    n2 = int(input("Ingrese otro número."))
+
+    porcentaje = (n1*n2)/100
+
+    print("El promedio es:%", porcentaje)
+Ejercicio8()
